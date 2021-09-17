bfc734133c39a0a7451dde91ac567045758d7c4f
PanDonut <krystian.wrobel06@gmail.com>
2021-09-17 16:02:05 +0200

    dev

---
+++ src/component/footer/footer-left.js
@@ -0,0 +1,45 @@
+import { connect } from "react-redux";
+import * as Icons from '../icons';
+import TextRegularM from '../text/text-regular-m';
+import IconButton from '../buttons/icon-button';
+
+import styles from "./footer-left.module.css";
+
+function FooterLeft(props){
+    return (
+        <div className={styles.footerLeft}>
+            <ImgBox 
+                trackData={props.trackData}
+            />
+            <SongDetails 
+                trackData={props.trackData}
+            />
+        </div>
+    );
+}
+
+function ImgBox({ trackData }){
+    return (
+        <div className={styles.imgBox}>
+            <img src={trackData.trackImg} alt="Gavurlar"/>
+        </div>
+    );
+}
+
+function SongDetails({ trackData }){
+    return (
+        <div className={styles.songDetails}>
+            <TextRegularM>{trackData.trackName}</TextRegularM>
+            <TextRegularM><small>{trackData.trackArtist}</small></TextRegularM>
+        </div>
+    );
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+      trackData: state.trackData
+    };
+};
+  
+export default connect(mapStateToProps)(FooterLeft);
---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+import * as Icon from '../component/icons'
+import React from 'react'
+
+export default {
+  MOBILE_SIZE: 640,
+}
+
+export const MENU = [
+  {
+    title: 'Główna',
+    path: '/',
+    icon: <Icon.Home />,
+    iconSelected: <Icon.HomeActive />
+  }
+]
+
+export const PLAYLISTBTN = [
+]
+
+export const LIBRARYTABS = [
+]
