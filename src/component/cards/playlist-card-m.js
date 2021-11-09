39819af6c25a1428c139407545d4e4fbf941ac09
PanDonut <81489726+PanDonut@users.noreply.github.com>
2021-11-09 16:42:09 +0100

    dev

--- src/component/cards/playlist-card-m.js
+++ src/component/cards/playlist-card-m.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import TextBoldL from "../text/text-bold-l";
 import TextRegularM from '../text/text-regular-m';
 import PlayButton from '../buttons/play-button';
-
+import FadeIn from 'react-fade-in';
 import styles from "./playlist-card-m.module.css";
 
 function PlaylistCardM(props) {
@@ -18,7 +18,7 @@ function PlaylistCardM(props) {
 	return (
 		<div className={styles.PlaylistCardSBox}>
 			<Link to={`/playlist/${props.data.link}`}>
-				<div className={styles.PlaylistCardS}>
+				<FadeIn visible="true" delay="100" className={styles.PlaylistCardS}>
 					<div className={styles.ImgBox}>
 						<img src={props.data.imgUrl} alt={props.data.title} />
 					</div>
@@ -26,7 +26,7 @@ function PlaylistCardM(props) {
 						<TextBoldL>{props.data.title}</TextBoldL>
 						<TextRegularM>{props.data.artist}</TextRegularM>
 					</div>
-				</div>
+				</FadeIn>
 			</Link>
 			<div 
 				onClick={() => props.changeTrack([parseInt(props.data.index), 0])} 
