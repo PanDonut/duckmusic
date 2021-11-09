5aa10d5c59580a77ead7174872c1477830c922db
PanDonut <81489726+PanDonut@users.noreply.github.com>
2021-11-09 13:00:49 +0100

    Update music-control-box-phone.js

--- src/component/footer/player/music-control-box-phone.js
+++ src/component/footer/player/music-control-box-phone.js
@@ -14,13 +14,37 @@ function MusicControlBox(props) {
     const [shuffling, setShuffling] = React.useState(localStorage.getItem('shuffle'));
 
     function decreaseIndex(){
-        if(props.trackData.trackKey[1] == 0){ }else{
-            props.changeTrack([props.trackData.trackKey[0], props.trackData.trackKey[1]-1])
+        if (localStorage.getItem('shuffle') == 'false') {
+            if (props.trackData.trackKey[1] === (PLAYLIST[props.trackData.trackKey[0]].playlistData.length)) {
+                props.changeTrack([props.trackData.trackKey[0], 0])
+            } else {
+                props.changeTrack([props.trackData.trackKey[0], parseInt(props.trackData.trackKey[1]) - 1])
+            }
+        } else if (localStorage.getItem('shuffle') == 'true') {
+            if (props.trackData.trackKey[1] === (PLAYLIST[props.trackData.trackKey[0]].playlistData.length)) {
+                props.changeTrack([props.trackData.trackKey[0], 0])
+            } else {
+                props.changeTrack([props.trackData.trackKey[0], parseInt(props.trackData.trackKey[1]) - 1])
+            }
+        } else {
+            localStorage.setItem('shuffle', 'false')
         }
     }
     function increaseIndex(){
-        if(props.trackData.trackKey[1] == (PLAYLIST[props.trackData.trackKey[0]].playlistData.length)-1){ }else{
-            props.changeTrack([props.trackData.trackKey[0], parseInt(props.trackData.trackKey[1])+1])
+        if (localStorage.getItem('shuffle') == 'false') {
+            if (props.trackData.trackKey[1] === (PLAYLIST[props.trackData.trackKey[0]].playlistData.length)) {
+                props.changeTrack([props.trackData.trackKey[0], 0])
+            } else {
+                props.changeTrack([props.trackData.trackKey[0], parseInt(props.trackData.trackKey[1]) + 1])
+            }
+        } else if (localStorage.getItem('shuffle') == 'true') {
+            if (props.trackData.trackKey[1] === (PLAYLIST[props.trackData.trackKey[0]].playlistData.length)) {
+                props.changeTrack([props.trackData.trackKey[0], 0])
+            } else {
+                props.changeTrack([props.trackData.trackKey[0], Math.floor((Math.random() * parseInt(PLAYLIST[props.trackData.trackKey[0]].playlistData.length)) + 0)])
+            }
+        } else {
+            localStorage.setItem('shuffle', 'false')
         }
     }
 
