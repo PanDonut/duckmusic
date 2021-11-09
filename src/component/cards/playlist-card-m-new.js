3f78e72d8d6132335c42138ebdcdaa68bb83b2fc
PanDonut <81489726+PanDonut@users.noreply.github.com>
2021-11-09 16:40:51 +0100

    dev

--- src/component/cards/playlist-card-m-new.js
+++ src/component/cards/playlist-card-m-new.js
@@ -6,6 +6,8 @@ import TextBoldL from "../text/text-bold-l";
 import TextRegularM from '../text/text-regular-m';
 import PlayButton from '../buttons/play-button';
 
+import FadeIn from 'react-fade-in';
+
 import styles from "./playlist-card-m-new.module.css";
 
 function PlaylistCardM(props) {
@@ -18,18 +20,18 @@ function PlaylistCardM(props) {
 	return (
 		<div className={styles.PlaylistCardSBox}>
 			<Link to={`/playlist/${props.data.link}`}>
-				<div className={styles.PlaylistCardS}>
+				<FadeIn visible="true" delay="50" className={styles.PlaylistCardS}>
 					<div className={styles.ImgBox}>
 						<img src={props.data.imgUrl} alt={props.data.title} />
 					</div>
-					<div className={styles.Title}>
+					<FadeIn visible="true" delay="100" className={styles.Title}>
 						<TextBoldL>{props.data.title}</TextBoldL>
 						<TextRegularM>{props.data.type.toUpperCase()} <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
 							viewBox="0 0 31.955 31.955" width="5px" height="5px">
 							<circle cx="10" cy="1" r="6.117" />
 						</svg>
  {props.data.artist}</TextRegularM>
-					</div>
+					</FadeIn>
 				</div>
 			</Link>
 			<div 
