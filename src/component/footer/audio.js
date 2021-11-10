e15f9e3053914669a9a21d1c9975927e1c3a0318
PanDonut <81489726+PanDonut@users.noreply.github.com>
2021-11-10 13:39:24 +0100

    dev

--- src/component/footer/audio.js
+++ src/component/footer/audio.js
@@ -16,6 +16,7 @@ const Audio = forwardRef(({ trackData, handleDuration, handleCurrentTime, isPlay
                 onTimeUpdate={(e) => handleCurrentTime(e.target.currentTime)}
                 src={trackData.track}
                 preload='auto'
+                autoplay
                 />
             </div>
     );
