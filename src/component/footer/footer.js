a6c92eb1eb15a0b692dd4785db8a98927e73d5ee
PanDonut <krystian.wrobel06@gmail.com>
2021-11-26 19:21:20 +0100

    dev

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -323,27 +323,6 @@ document.documentElement.style.setProperty('--txtpos', "translateX(0px)");
                     <FooterLeft />
                 </div>
                 
-                    {size.width > CONST.MOBILE_SIZE &&
-                    <div className={styles.footerMid}>
-                    <MusicControlBox/>
-                    <MusicProgressBar
-                        currentTime={currentTime}
-                        duration={duration}
-                        handleTrackClick={handleTrackClick}
-                    />
-                    </div>
-                }
-                {size.width < CONST.MOBILE_SIZE &&
-                    <FadeIn visible="true" delay="500" className={styles.footerMe}>
-                    <MusicProgressBarF
-                        currentTime={currentTime}
-                        duration={duration}
-                        handleTrackClick={handleTrackClick}
-                    />
-                        <MusicControlBoxPhone />
-                                
-                    </FadeIn>
-                }
                     
                    
 
@@ -363,14 +342,12 @@ document.documentElement.style.setProperty('--txtpos', "translateX(0px)");
                     />
                 
             </div>
-            {size.width < CONST.MOBILE_SIZE &&
                 <div className={styles.footerMid}>
                     <MusicProgressBarBot
                         currentTime={currentTime}
                         duration={duration}
                     />
                 </div>
-            }
             <button id="bt1" className="exp" onClick={() => { Expand2() }}>
                 <Icons.Prevpage />
             </button>
