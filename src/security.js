7c109921b898b7c22d576cc5f4f1f60109b17d81
PanDonut <krystian.wrobel06@gmail.com>
2021-12-17 15:15:22 +0100

    dev

--- src/security.js
+++ src/security.js
@@ -13,13 +13,8 @@ const infobtnCSS = 'color:white; font-family: "Arial Black", Gadget, sans-serif;
 
 console.log('%cUwaga!', warningTitleCSS);
 console.log("%cJeżeli ktoś powiedział Ci żeby coś tu wkleić to na 99.9% próbuje Cię oszukać. Wklejając tu coś ktoś może uzyskać dostęp do twojego konta lub urządzenia.", warningDescCSS);
-console.log("%cJeżeli doskonale wiesz co robisz dołącz do TheDuck Studios 😃", infoDescCSS);
-console.log("%cKonsola to ciemne i zimne miejsce...", warningDescCSS);
-    console.log("Dowiedz się więcej", infobtnCSS);
 
 window.setInterval(function () {
     console.log('%cUwaga!', warningTitleCSS);
     console.log("%cJeżeli ktoś powiedział Ci żeby coś tu wkleić to na 99.9% próbuje Cię oszukać. Wklejając tu coś ktoś może uzyskać dostęp do twojego konta lub urządzenia.", warningDescCSS);
-    console.log("%cJeżeli doskonale wiesz co robisz dołącz do TheDuck Studios 😃", infoDescCSS);
-    console.log("%cKonsola to ciemne i zimne miejsce...", warningDescCSS);
 }, 10000); // 10000 milisekund (10 sekund)
