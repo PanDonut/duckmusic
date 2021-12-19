db62c613f1765edb2fdad686bb7c21115ac4f84b
PanDonut <81489726+PanDonut@users.noreply.github.com>
2021-12-19 11:17:39 +0100

    Update Lyrics.js

--- src/component/Lyrics.js
+++ src/component/Lyrics.js
@@ -202,6 +202,14 @@ function Lyrics({ song, currentTime, songId, sly }) {
                                 setIsTranslated(true);
                                 const url = `/pl/lyrics/${song.trackName.split(" ").map(piece => piece.toLowerCase()).join("-")}.json`
                                 axios.get(url)
+.then(res => {
+                                        if (res != undefined) {
+                                            lyrics = res.data;
+                                            setLy(res.data);
+                                            setState.lyrics(lyrics);
+                                            dispatch({ type: actions.SET_LYRICS, payload: { id: songId, lyrics } })
+                                        }
+                                    })
                                     .catch(e => {
                                         lyrics = [{
                                             "start": 0,
@@ -222,15 +230,7 @@ function Lyrics({ song, currentTime, songId, sly }) {
                                             "lyrics": e.toString()
                                         }]);
                                         setState.lyrics(lyrics);
-                                    })
-                                    .then(res => {
-                                        if (res != undefined) {
-                                            lyrics = res.data;
-                                            setLy(res.data);
-                                            setState.lyrics(lyrics);
-                                            dispatch({ type: actions.SET_LYRICS, payload: { id: songId, lyrics } })
-                                        }
-                                    })
+                                   }}            
                             }
                         }}><svg width="15px" height="15px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Przetłumacz</title><path d="M7.41 9l2.24 2.24-.83 2L6 10.4l-3.3 3.3-1.4-1.42L4.58 9l-.88-.88c-.53-.53-1-1.3-1.3-2.12h2.2c.15.28.33.53.51.7l.89.9.88-.88C7.48 6.1 8 4.84 8 4H0V2h5V0h2v2h5v2h-2c0 1.37-.74 3.15-1.7 4.12L7.4 9zm3.84 8L10 20H8l5-12h2l5 12h-2l-1.25-3h-5.5zm.83-2h3.84L14 10.4 12.08 15z" /></svg></button> : <button className="button-translate-active" onClick={() => {
                             if (lyrics.length < 1) {
